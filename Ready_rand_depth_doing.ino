94a2c55aeb8297de6c70b9087f73c480d5f77d67
warior-d <haloy@yandex.ru>
2022-08-26 15:27:13 +0300

    before big deal

--- Ready_rand_depth_doing.ino
+++ Ready_rand_depth_doing.ino
@@ -4,6 +4,26 @@ SoftwareSerial ss(4, 5);
 
 float GLOBAL_DEPTH;
 
+const char *coord[] = {
+"55.069531,38.821459",
+"55.069697,38.820460",
+"55.069854,38.819533",
+"55.069979,38.818661",
+"55.069958,38.817916",
+"55.069729,38.816026",
+"55.069572,38.815481",
+"55.069166,38.815063",
+"55.068884,38.815245",
+"55.068676,38.815590",
+"55.068551,38.816335",
+"55.068509,38.817244",
+"55.068530,38.817970",
+"55.068561,38.819042",
+"55.068738,38.820169",
+"55.069291,38.820986"
+};
+
+
 /*
   char* myStrings[]={
   "$GPGLL,5540.91588,N,03752.72131,E,211606.00,A,A*67",
@@ -47,6 +67,9 @@ float GLOBAL_DEPTH;
 */
 boolean READY_WRITE = false;
 
+#define DEBUG 0
+
+
 void setup() {
   Serial.begin(9600);
   ss.begin(9600);
@@ -56,12 +79,53 @@ void setup() {
 
 void loop() {
 
-  if (ss.available() > 0) {
-    char c = ss.read();
-    PrintString(c);
-    //Serial.write(c);
+
+
+  Serial.println("$GPGLL,5504.10546,N,03848.80004,E,211606.00,A,A*67");
+  delay(1000);
+/*
+if (DEBUG == 1){
+  char cur_coord[19];
+  for(int i=0; i < sizeof(coord)/2; i++){
+    Serial.println(sizeof(coord[i]));
+    delay(1000);
+    char buf;
+    
+    int y = 0;
+    int n = 0;
+    Serial.println(strlen(coord[i]));
+    for(int t = 0; t <= strlen(coord[i]); t++){
+      buf = coord[i][t];
+      if((buf != '\0') && (buf != ',')){
+        cur_coord[y] = buf;
+        
+        y++;
+      }
+      else if(buf == ','){
+        Serial.println(cur_coord);
+        y = 0;
+      }
+      else if(buf == '\0'){
+        Serial.println(cur_coord);
+        y = 0;
+        
+      }
+    }
+
+    
   }
   
+  //Serial.println("$GPGLL,5504.10546,N,03848.80004,E,211606.00,A,A*67");
+  delay(1000);
+}
+else{*/
+  if (ss.available() > 0) {
+    
+    char c = ss.read();
+    PrintString(c);
+
+  }
+
 }
 
 void getDepth()
